e39c0c33f0550a11d11087fbd6ea308fabf44bcf
VitaliiGorovoii <vitalii.gorovoii@gmail.com>
2017-04-29 13:37:39 +0300

    Initial commit

---
+++ app/src/main/java/com/gorovoii/vitalii/fbdb/activity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.gorovoii.vitalii.fbdb.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.gorovoii.vitalii.fbdb.R;
+import com.gorovoii.vitalii.fbdb.adapter.NoteAdapter;
+import com.gorovoii.vitalii.fbdb.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private NoteAdapter mNoteAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.actionAdd) {
+            Intent intent = new Intent(this, AddNoteActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void init() {
+        List<Note> data = new ArrayList<>();
+        data.add(new Note("Zametka", "body"));
+        data.add(new Note("Zametka1", "body"));
+        data.add(new Note("Zametka2", "body"));
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler);
+        mNoteAdapter = new NoteAdapter(this,data);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mNoteAdapter);
+
+    }
+
+}
---
+++ app/src/main/java/com/gorovoii/vitalii/fbdb/adapter/NoteAdapter.java
@@ -0,0 +1,60 @@
+package com.gorovoii.vitalii.fbdb.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gorovoii.vitalii.fbdb.R;
+import com.gorovoii.vitalii.fbdb.model.Note;
+
+import java.util.List;
+
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
+
+    private List<Note> dataList;
+    private Context mContext;
+
+    public NoteAdapter(Context context, List<Note> data) {
+        this.dataList = data;
+        this.mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.card_note, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView title;
+        private TextView body;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView)itemView.findViewById(R.id.title);
+            body = (TextView) itemView.findViewById(R.id.body);
+        }
+
+        void bind(final Note note) {
+            title.setText(note.getTitle());
+            body.setText(note.getBody());
+        }
+    }
+
+}
+
