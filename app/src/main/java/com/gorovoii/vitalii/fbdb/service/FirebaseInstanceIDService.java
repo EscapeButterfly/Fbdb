406922d7554030cb4312929126cf322f80db0fba
joulis1derful <joulis1derful@gmail.com>
2017-05-24 18:11:49 +0300

    Cleared unimportant things

--- app/src/main/java/com/gorovoii/vitalii/fbdb/service/FirebaseInstanceIDService.java
+++ app/src/main/java/com/gorovoii/vitalii/fbdb/service/FirebaseInstanceIDService.java
@@ -5,38 +5,16 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-
-
 public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     private static final String TAG = "MyFirebaseIIDService";
-//    private static String fileName = "C://Users//User//Desktop//a.txt";
 
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken);
-    }
-    /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM InstanceID token with any server-side account
-     * maintained by your application.
-     *
-     * @param token The new token.
-     */
-    private void sendRegistrationToServer(String token) {
+        Log.e(TAG, "Refreshed token: " + refreshedToken);
 
     }
+
     }
