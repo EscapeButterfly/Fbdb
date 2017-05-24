4694acf0d9920824ad9b7d2cc6809397a9442ecd
joulis1derful <joulis1derful@gmail.com>
2017-05-24 18:12:31 +0300

    Added new service to send notifications while sending a post

---
+++ app/src/main/java/com/gorovoii/vitalii/fbdb/service/DevToDevIntent.java
@@ -0,0 +1,79 @@
+package com.gorovoii.vitalii.fbdb.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class DevToDevIntent extends IntentService {
+
+    private static final String TOKEN =
+            "cL0zizW4Opk:APA91bGguwz24W0wutvJc1ZAKdY2hQhNCD0Yk3td_GOnQ60Q3SFapWKNsacFR8vZ7w6PsowYhc2Fsmx1-DeLw3meRpnLPqh3niGE1ltcef27mdgLA4XyjU0gTN0QDpFcCnefSOpIYSEQ";
+
+    public static final String LEGACY_SERVER_KEY =
+            "AAAASKprTh8:APA91bG2Q6DhRf6vNLZ7FRQ6cJyAIu7OsPRau8BsO8pbSQk6WphOqmGU_MfRLLmUK15g50iu4S2mY96SmIOgCS9kTpBJHuAGLRDiUDlCfMON19IGSzlbztqWrAwFL8FQf478YxIJgQ-B";
+
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    public DevToDevIntent() {
+        super("serviceMessage");
+    }
+
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //send message
+        Log.e("IntentService", "handling");
+        try {
+
+            OkHttpClient client = new OkHttpClient();
+            JSONObject json=new JSONObject();
+            JSONObject dataJson=new JSONObject();
+            dataJson.put("body","Someone has created a note");
+            dataJson.put("title","UPDATE");
+            json.put("notification", dataJson);
+            json.put("to",TOKEN);
+            RequestBody body = RequestBody.create(JSON, json.toString());
+            Request request = new Request.Builder()
+                    .header("Authorization","key="+ LEGACY_SERVER_KEY)
+                    .url("https://fcm.googleapis.com/fcm/send")
+                    .post(body)
+                    .build();
+//            Log.e("URL from builder:", request.toString().substring(0,20));
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Log.e("intentService", "failure");
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    Log.e("intentService", "success");
+
+                    Log.e("URL:", call.request().toString());
+                }
+            });
+
+        }catch (Exception e) {
+            Log.d("Exception", e + "");
+            e.printStackTrace();
+        }
+    }
+}
